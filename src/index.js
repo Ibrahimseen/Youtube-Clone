7c4a5bc656375ecde385e01ef9ec6771aa4d5ee2
Ibrahimseen <ibrahimrehmanibrahim@gmail.com>
2024-02-02 19:09:32 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
+// Increase the max listeners for EventEmitter
+require('events').EventEmitter.defaultMaxListeners = 15;
+
+// Render the App inside BrowserRouter
 ReactDOM.render(
   <BrowserRouter>
     <App />
@@ -12,5 +16,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+// Report web vitals for performance metrics
 reportWebVitals();
